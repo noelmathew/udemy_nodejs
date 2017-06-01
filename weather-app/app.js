da99c0c1b5e606df00d118d5715b6023615462df
Noel <noelmathew@gmail.com>
2017-05-31 20:08:38 -0400

    Lesson 32: Error handling for weather app

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,8 +1,38 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+	.options({
+		a: {
+			demand: true,
+			alias: 'address',
+			describe: 'Address to fetch weather for',
+			string: true
+		}
+	})
+	.help()
+	.alias('help','h')
+	.argv;
+
+var encodedAddress = encodeURIComponent(argv.address);
 
 request({
-	url: 'http://maps.googleapis.com/maps/api/geocode/json?address=181%2010th%20Street%20Jersey%20City',
+	url: `http://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
 	json: true
 },(error, response, body) => {
-	console.log(JSON.stringify(body, undefined, 2));
+	if(error){
+		console.log("Unable to connect to Google Servers");
+	}
+	else if(body.status === "ZERO_RESULTS"){
+		console.log(`No value found with the given address (${argv.address})`);
+	}
+	else if(body.status === "OK")
+	{
+		console.log(`Address: ${body.results[0].formatted_address}`);
+		console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+		console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+	}
+	else{
+		console.log('Unknown error');
+	}
 });
